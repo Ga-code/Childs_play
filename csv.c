847774d7c3adb234346bdb539c6d375c73f79c0e
Agnosto Dvonik <dvonik@rslp.org>
2022-10-11 19:26:50 -0400

    memory poking experiment

--- csv.c
+++ csv.c
@@ -1,7 +1,22 @@
+#include <stdio.h>
 #include <string.h>
 #include "csv.h" 
 
-void
-*parsething(struct *save, char *in)
+int
+main(void)
 {
+	save mysave;
+	int a, b;
+
+	a = sizeof(mysave);
+
+	char name[] = "Wow";
+
+	strcpy(mysave.name, name);
+
+	printf("mysave 0x%02x\n", &mysave);
+	printf("mysave name\t0x%02x %s\n", &mysave.name, mysave.name);
+	printf("mysave data1\t0x%02x %s\n", &mysave.data1, mysave.data1);
+
+	printf("offset 0x%02x\n", mysave.data1 - mysave.name);
 }
