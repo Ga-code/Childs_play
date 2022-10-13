5ee768f2a79bc2876a4883ebdd9eb7138e1fed1e
Losing_Pro <gauthamraajesh@gmail.com>
2022-10-13 19:17:30 -0400

    basic variables day 1

--- csv.h
+++ csv.h
@@ -2,9 +2,18 @@
 #define CSV_H
 
 typedef struct save {
-	char name[32];
-	char data1[32];
-	char data2[32];
+	char type; /* the type you have(there is 9 of them)*/
+	int population; /* the amount of people you have*/
+	int nation_morale; /* this type of morale affectes soldier production and gets affected by taxes*/
+	int iron_resource; /* the amount of iron you currently have */
+	int iron_production; /* number of iron produced each turn(round)*/
+	int wood_resource; /* the amount of wood you currently have */
+	int wood_production; /* number of wood produced each turn(round)*/
+	int stone_resource; /* the amount of stone you currently have */
+	int stone_production; /* number of stone produced each turn(round)*/
+
+	
+	
 } save;
 
 #endif
